582c38266f9a37b014581d9c4cdaa26436e52cd4
dabaishu <1250709351@qq.com>
2020-08-02 00:01:26 +0800

    主页布局和大体样式完成

--- src/plugins/element.js
+++ src/plugins/element.js
@@ -1,7 +1,11 @@
 import Vue from 'vue'
 import {
-    Button, Container, Switch, Header, Aside, Main, Row,
-    Message
+    Button, Container, Switch,
+    Header, Aside, Main, Row, Col,
+    Breadcrumb, BreadcrumbItem,
+    Tabs, TabPane,
+    Menu, Submenu, MenuItem, MenuItemGroup,
+    Message,
 } from 'element-ui'
 
 Vue.use(Button)
@@ -11,4 +15,13 @@ Vue.use(Header)
 Vue.use(Aside)
 Vue.use(Main)
 Vue.use(Row)
+Vue.use(Col)
+Vue.use(Breadcrumb)
+Vue.use(BreadcrumbItem)
+Vue.use(TabPane)
+Vue.use(Tabs)
+Vue.use(Menu)
+Vue.use(Submenu)
+Vue.use(MenuItem)
+Vue.use(MenuItemGroup)
 Vue.prototype.$message = Message
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -5,10 +5,42 @@
         <div class="title">
           安软标记平台
         </div>
+        <div class="tab-box">
+          <el-row class="tac">
+            <el-menu
+              default-active="1"
+              class="el-menu-vertical-demo"
+              background-color="transparent"
+              text-color="#fff"
+              active-text-color="#ffd04b">
+              <el-menu-item index="1">
+                <i class="el-icon-menu"></i>
+                <span slot="title">全部项目</span>
+              </el-menu-item>
+              <el-menu-item index="2">
+                <i class="el-icon-user-solid"></i>
+                <span slot="title">个人中心</span>
+              </el-menu-item>
+              <el-menu-item index="3">
+                <i class="el-icon-s-tools"></i>
+                <span slot="title">管理中心</span>
+              </el-menu-item>
+            </el-menu>
+          </el-row>
+        </div>
       </el-aside>
       <el-container>
-        <el-header>Header</el-header>
-        <el-main>Main</el-main>
+        <el-header height="40px">
+          <el-breadcrumb separator-class="el-icon-arrow-right">
+            <el-breadcrumb-item :to="{ path: '/home' }">首页</el-breadcrumb-item>
+            <el-breadcrumb-item>活动管理</el-breadcrumb-item>
+            <el-breadcrumb-item>活动列表</el-breadcrumb-item>
+            <el-breadcrumb-item>活动详情</el-breadcrumb-item>
+          </el-breadcrumb>
+        </el-header>
+        <el-main>
+
+        </el-main>
       </el-container>
     </el-container>
   </div>
@@ -32,11 +64,43 @@ export default {
 <style lang="less" scoped>
   .home-box{
     height: 100%;
-  }
-  .el-container{
-    height: 100%;
-  }
-  .el-aside{
-    background-color: #134263;
+    .el-container{
+      height: 100%;
+    }
+    .el-aside{
+      background-color: #3c4147;
+      .title{
+        width: 100%;
+        height: 80px;
+        background-color: #272b34;
+        text-align: center;
+        line-height: 80px;
+        color: #eeeeee;
+        font-size: 22px;
+      }
+      .tab-box{
+        width: 100%;
+        padding-top: 20px;
+        .el-menu-vertical-demo{
+          width: 100%;
+          .el-menu-item{
+            margin-bottom: 6px;
+          }
+          .el-menu-item:hover{
+            background-color: #0E2F46 !important;
+          }
+        }
+      }
+    }
+    .el-header{
+      background-color: #51b5ff+;
+      .el-breadcrumb{
+        line-height: 40px;
+        font-size: 16px;
+      }
+    }
+    .el-main{
+      background-color: #eeeeee;
+    }
   }
 </style>
